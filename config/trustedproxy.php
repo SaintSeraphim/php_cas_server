45a1bef6933a001ebf4f5a03cc9d2348bd57e11a
Saint Seraphim <juliana@tic.ufrj.br>
2022-07-13 16:42:45 -0300

    Update trustedproxy.php

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -33,9 +33,9 @@ return [
      * \Symfony\Component\HttpFoundation\Request::$trustedHeaders
      */
     'headers' => [
-        \Illuminate\Http\Request::HEADER_CLIENT_IP    => env('TRUSTED_HEADER_CLIENT_IP', 'X_FORWARDED_FOR'),
-        \Illuminate\Http\Request::HEADER_CLIENT_HOST  => env('TRUSTED_HEADER_CLIENT_HOST', 'X_FORWARDED_HOST'),
-        \Illuminate\Http\Request::HEADER_CLIENT_PROTO => env('TRUSTED_HEADER_CLIENT_PROTO', 'X_FORWARDED_PROTO'),
-        \Illuminate\Http\Request::HEADER_CLIENT_PORT  => env('TRUSTED_HEADER_CLIENT_PORT', 'X_FORWARDED_PORT'),
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR => env('TRUSTED_HEADER_CLIENT_IP', 'X_FORWARDED_FOR'),
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST  => env('TRUSTED_HEADER_CLIENT_HOST', 'X_FORWARDED_HOST'),
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO => env('TRUSTED_HEADER_CLIENT_PROTO', 'X_FORWARDED_PROTO'),
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT  => env('TRUSTED_HEADER_CLIENT_PORT', 'X_FORWARDED_PORT'),
     ],
 ];
